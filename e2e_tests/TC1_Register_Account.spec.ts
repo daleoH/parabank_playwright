010d7e06931d2a465139a3123808a6fa5c6697c7
Dale Harris <dale.harris2@justice.gov.uk>
2023-06-03 19:16:54 +0100

    refactor registration tests to include baseurl and new strings

--- e2e_tests/TC1_Register_Account.spec.ts
+++ e2e_tests/TC1_Register_Account.spec.ts
@@ -1,5 +1,7 @@
 import {test, expect} from "@playwright/test";
-import {generateName, generateNumber, generateRandomString} from "./utils";
+import {generateRandomString, generatePhoneNumber, generateSSN, generateFName, generateLName} from "./utils";
+
+const randomstring = require("randomstring");
 //this will be used as a test hook to be run on each test to ensure correct test set up
 /*test("Register user account setup", async ({page}) => {
     // Go to the starting url before each test.
@@ -11,7 +13,6 @@ import {generateName, generateNumber, generateRandomString} from "./utils";
     await page.locator('#initialBalance').click();
     await page.getByRole('button', {name: 'Submit'}).click();
 });*/
-
 test.describe(" Registration tests", async () => {
     test("User attempts to register account without entering mandatory data", async ({page,baseURL}) => {
         //navigate to parabank url, baseurl stored in config file
@@ -19,7 +20,6 @@ test.describe(" Registration tests", async () => {
         //click on the register link followed by the register button without entering any data on /register.htm page
         await page.getByRole('link', {name: 'Register'}).click();
         await page.getByRole('button', {name: 'Register'}).click();
-
         //Assertion for verifying each fields validation
         await expect(page.getByText('First name is required.')).toBeVisible();
         //another way to ensure the validation text is present on the page
@@ -36,30 +36,29 @@ test.describe(" Registration tests", async () => {
     });
 
 
-    test("User enters incorrect confirmation password - mismatch", async ({page}) => {
+    test("User enters incorrect confirmation password - mismatch", async ({page,baseURL}) => {
         //navigate to parabank url, baseurl stored in config file
-        await page.goto('https://parabank.parasoft.com/parabank/register.htm');
+        await page.goto(baseURL + 'register.htm');
         await page.locator('[id="customer.password"]').fill(generateRandomString(10));
         await page.locator('#repeatedPassword').fill(generateRandomString(10));
         await page.getByRole('button', {name: 'Register'}).click();
         await expect(page.locator('[id="repeatedPassword.errors"]')).toHaveText('Passwords did not match.');
     });
 
-    test("Successfully Register Account", async ({page}) => {
+    test("Successfully Register Account", async ({page,baseURL}) => {
         //navigate to parabank url, baseurl stored in config file
-        await page.goto('https://parabank.parasoft.com/parabank/register.htm');
-        //await page.locator('[id="customer.firstName"]').fill(randomFirstName);
-        await page.locator('[id="customer.firstName"]').fill(generateName);
-        await page.locator('[id="customer.lastName"]').fill(generateName);
+        await page.goto( baseURL + 'register.htm');
+        await page.locator('[id="customer.firstName"]').fill(generateFName);
+        await page.locator('[id="customer.lastName"]').fill(generateLName);
         await page.locator('[id="customer.address.street"]').fill(generateRandomString(9));
         await page.locator('[id="customer.address.city"]').fill(generateRandomString(10));
-        await page.locator('[id="customer.address.state"]').fill(generateRandomString(10));
-        await page.locator('[id="customer.address.zipCode"]').fill(generateRandomString(10));
-        await page.locator('[id="customer.phoneNumber"]').fill('077' + generateNumber);
-        await page.locator('[id="customer.ssn"]').fill(generateRandomString(10));
+        await page.locator('[id="customer.address.state"]').fill(generateRandomString(15));
+        await page.locator('[id="customer.address.zipCode"]').fill(generateRandomString(7));
+        await page.locator('[id="customer.phoneNumber"]').fill('077' + generatePhoneNumber);
+        await page.locator('[id="customer.ssn"]').fill(generateSSN);
         await page.locator('[id="customer.username"]').fill(generateRandomString(10));
         await page.locator('[id="customer.password"]').fill('test1');
         await page.locator('#repeatedPassword').fill('test1');
-        //await page.getByRole('button', {name: 'Register'}).click();
+        await page.getByRole('button', {name: 'Register'}).click();
     })
 });
